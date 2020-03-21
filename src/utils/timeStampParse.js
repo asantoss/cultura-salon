c0122bbfe83d29ac2d7cc8b0396a2d734da605c8
Alexander Santos <alexsantosantana@live.com>
2020-03-21 02:29:23 -0400

    Fixing small functions

--- src/utils/timeStampParse.js
+++ src/utils/timeStampParse.js
@@ -1,7 +1,7 @@
 export default function timeStampParse(timestamp) {
   const date = new Date(timestamp * 1000)
   const month = date.toLocaleString("default", { month: "long" })
-  const day = date.getDay()
+  const day = date.getDate()
   const year = date.getFullYear()
   return `${month} ${day}, ${year}`
 }
